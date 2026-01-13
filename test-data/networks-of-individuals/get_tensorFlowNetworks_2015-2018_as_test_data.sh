c774fbc7bddb8ba82ee1a8480d9e65e4df7c3073
Jose Teixeira <japolinario@gmail.com>
2026-01-13 09:37:44 +0200

    Jose

---
+++ test-data/networks-of-individuals/get_tensorFlowNetworks_2015-2018_as_test_data.sh
@@ -0,0 +1,14 @@
+echo Getting some networks https://github.com/jaateixeira/ScrapLogGit2Net/blob/main/test-data/TensorFlow/icis-2024-wp-networks-graphML/
+
+echo see https://users.aalto.fi/~apolinj2/tensorflowsna/ for more info 
+
+
+wget https://raw.githubusercontent.com/jaateixeira/ScrapLogGit2Net/refs/heads/main/test-data/TensorFlow/icis-2024-wp-networks-graphML/tensorFlowGitLog-2015-git-log-outpuyt-by-Jose.IN.NetworkFile.graphML
+
+wget https://raw.githubusercontent.com/jaateixeira/ScrapLogGit2Net/refs/heads/main/test-data/TensorFlow/icis-2024-wp-networks-graphML/tensorFlowGitLog-2015-git-log-outpuyt-by-Jose.IN.NetworkFile.graphML
+
+wget https://raw.githubusercontent.com/jaateixeira/ScrapLogGit2Net/refs/heads/main/test-data/TensorFlow/icis-2024-wp-networks-graphML/tensorFlowGitLog-2015-git-log-outpuyt-by-Jose.IN.NetworkFile.graphML
+
+wget https://raw.githubusercontent.com/jaateixeira/ScrapLogGit2Net/refs/heads/main/test-data/TensorFlow/icis-2024-wp-networks-graphML/tensorFlowGitLog-2015-git-log-outpuyt-by-Jose.IN.NetworkFile.graphML
+
+echo DONE
